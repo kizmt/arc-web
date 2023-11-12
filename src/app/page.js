fb7753bd2e96a2338932e7b5f82fc82a7ff6bce8
kizmt <kizmetcrypto@gmail.com>
2023-11-13 09:40:38 +1300

    fix breaks

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,30 @@
 "use client";
+import { useEffect, useState, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import styles from "./page.module.css";
 import Stars from "./components/3D/Stars";
 import Modal from "./components/3d/Modal";
 import Overlay from "./components/2D/Overlay/Overlay";
 import Loader from "./components/2D/Loader/Loader";
-import { Suspense } from "react";
 
 export default function Home() {
-  var x = window.matchMedia("(max-width: 700px)");
+  const [isMobile, setIsMobile] = useState(false); // Default to false or true based on your initial assumption
+
+  useEffect(() => {
+    const handleResize = () => {
+      const x = window.matchMedia("(max-width: 700px)");
+      setIsMobile(x.matches);
+    };
+
+    // Call once to set initial value
+    handleResize();
+
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <div className={styles.main_container}>
@@ -17,15 +33,14 @@ export default function Home() {
         <Canvas camera={{ position: [0, 0, 1] }}>
           <Stars />
           <Modal
-            position={x.matches === false ? [0.8, 0, 0] : [0.04, -0.09, 0]}
+            position={isMobile ? [0.04, -0.09, 0] : [0.8, 0, 0]}
             rotation={
-              x.matches === false
-                ? [0, Math.PI * -0.2, 0]
-                : [0, Math.PI * -0.08, 0]
+              isMobile
+                ? [0, Math.PI * -0.08, 0]
+                : [0, Math.PI * -0.2, 0]
             }
-            scale={x.matches === false ? 0.0095 : 0.0075}
+            scale={isMobile ? 0.0075 : 0.0095}
           />
-
           <pointLight position={[100, 100, 100]} intensity={0.8} />
           <hemisphereLight
             color="#234fad"
